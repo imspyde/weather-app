fff021f9d4f9975bc374bc5915579df355f756c4
imspyde <67168764+imspyde@users.noreply.github.com>
2020-06-25 12:07:57 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geoCode = require('./Utils/geoCode')
 
 
 const app = express()
-
+const port = process.env.PORT || 3000
 //Defining paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -96,6 +96,6 @@ app.get('*',(req, res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is Started! on port')
+app.listen(port,()=>{
+    console.log('Server is Started! on port' + port)
 })
